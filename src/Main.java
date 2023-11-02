df0c018d66f254b1a92f909b805854734febe530
yeaokfr <144458128+yeaokfr@users.noreply.github.com>
2023-11-01 21:50:28 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String symbol = "*";
+        //Regular triangle
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+        for (int i = 5; i >= 1; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("");
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
